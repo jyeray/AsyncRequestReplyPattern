0a7a44c0e01706846b82a7c664e0605fa797dce2
Joaquín Yeray Martín de la Nuez <joaquinmn@protonmail.com>
2021-06-12 20:26:39 +0100

    uadd gate function url as command parameter

--- AsyncRequestReply.Client/Program.cs
+++ AsyncRequestReply.Client/Program.cs
@@ -5,28 +5,32 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.VisualBasic;
+using Microsoft.Extensions.Configuration;
 
 namespace AsyncRequestReply.Client
 {
     class Program
     {
         private static readonly HttpClient client = new HttpClient();
-        static async Task Main()
+
+        static async Task Main(string[] args)
         {
+            var configuration = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
             Console.WriteLine("Please enter a name:");
             var name = Console.ReadLine();
-            var statusUrl = await CallGateFunction(name);
+            var statusUrl = await CallGateFunction(configuration["gateUrl"], name);
             var statusHttpReponseMessage = await CallStatusFunctionUntilResourceCreated(statusUrl);
             var response = await statusHttpReponseMessage.Content.ReadAsStringAsync();
             Console.WriteLine();
             Console.WriteLine($"Resource created, the resulting message is: '{response}'");
         }
 
-        public static async Task<string> CallGateFunction(string name) {
+        public static async Task<string> CallGateFunction(string gateUrl, string name) {
             Console.WriteLine();
-            Console.WriteLine("Making api call to Gate function");
-            var gateHttpResponseMessage = await client.PostAsJsonAsync("yourGateFunctionUrlHereWithAuthCode", new RequestBody{Name = name});
+            Console.WriteLine($"Making api call to Gate function: {gateUrl}");
+            var gateHttpResponseMessage = await client.PostAsJsonAsync(gateUrl, new RequestBody{Name = name});
             var gateResponse = await gateHttpResponseMessage.Content.ReadAsStringAsync();
             Console.WriteLine($"Gate function responded with http code: '{gateHttpResponseMessage.StatusCode}' and message: '{gateResponse}'");
             return gateHttpResponseMessage.Headers.GetValues("Location").First();
